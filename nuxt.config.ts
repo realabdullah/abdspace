b8292251a2179ebc466da7f377799bc79923cf09
realabdullah <abdulodesanmi@gmail.com>
2023-10-03 19:28:17 +0100

    (chore): sass setup 🚀

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    css: ["~/assets/sass/main.scss"],
+    vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+						@import "@/assets/sass/abstracts/_variables.scss"; 
+						@import "@/assets/sass/abstracts/_mixins.scss";
+					`,
+				},
+			},
+		},
+	},
+    devServer: {
+		port: 2000,
+	},
+    devtools: { enabled: true }
 })
