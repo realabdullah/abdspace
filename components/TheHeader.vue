b560d32f66ac53dc3b89338848e2f3e8fb793ac0
realabdullah <abdulodesanmi@gmail.com>
2023-10-08 13:59:27 +0100

    (fix): get spotify access token

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -5,8 +5,8 @@ const playingData = ref({
 	title: "",
 	link: "",
 	cover_art: "",
-    l_cover_art: "",
-    artists: "",
+	l_cover_art: "",
+	artists: "",
 });
 
 const getCurrentlyPlaying = async () => {
@@ -39,19 +39,24 @@ onMounted(async () => {
 			<IconsSpotify v-else />
 			<span v-if="isCurrentlyPlaying" class="text-five col-text">
 				Listening to
-				<button class="col-text cursor-pointer" @click="isModalOpen = !isModalOpen">
+				<button class="bg-transparent col-text cursor-pointer" @click="isModalOpen = !isModalOpen">
 					<strong>{{ playingData.title }}</strong>
 				</button>
 			</span>
 			<span v-else class="text-five col-text">Not listening atm 🫣</span>
 
-			<div v-show="isModalOpen" class="playing__modal position-absolute z-2 d-flex items-center gap-6 w-auto h-auto">
-                <img :src="playingData.l_cover_art" alt="cover_art">
-                <div class="details d-flex flex-column gap-2">
-                    <a :href="playingData.link" target="_blank" class="title text-five weight-500 col-text">{{ playingData.title }}</a>
-                    <p class="artists text-nowrap text-five weight-400 col-text">{{ playingData.artists }}</p>
-                </div>
-            </div>
+			<div v-if="isModalOpen" class="playing__modal position-absolute z-2 d-flex items-center gap-6 w-auto h-auto">
+				<img :src="playingData.l_cover_art" alt="cover_art" />
+				<div class="details d-flex flex-column gap-2">
+					<a :href="playingData.link" target="_blank" class="title text-five weight-500 col-text">{{ playingData.title }}</a>
+					<p class="artists text-nowrap text-five weight-400 col-text">{{ playingData.artists }}</p>
+				</div>
+				<div class="bar d-flex content-between ml-5">
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+			</div>
 		</div>
 	</header>
 </template>
@@ -75,42 +80,71 @@ onMounted(async () => {
 		.cover_art {
 			@include sizing(2rem, 2rem);
 		}
+
+		button {
+			@include ellipsis;
+		}
 	}
 }
 
 .playing__modal {
 	padding: 1rem;
-    padding-right: 3rem;
+	padding-right: 3rem;
 	box-shadow: 0 1rem 3rem rgba(#8d9097, 0.05);
 	top: 4rem;
-    right: 0;
-    opacity: 0;
-    animation: ellastic 1s ease-out forwards 0.15s;
-    background-color: var(--white);
-    backdrop-filter: blur(1rem);
+	right: 0;
+	opacity: 0;
+	animation: ellastic 1s ease-out forwards 0.15s;
+	background-color: var(--white);
+	backdrop-filter: blur(1rem);
 
-    img {
-        @include sizing(5rem, 5rem);
-        object-fit: cover;
-        border-radius: 0.5rem;
-        box-shadow: #64646f33 0 0.7rem 2.9rem 0;
-    }
+	img {
+		@include sizing(5rem, 5rem);
+		object-fit: cover;
+		border-radius: 0.5rem;
+		box-shadow: #64646f33 0 0.7rem 2.9rem 0;
+	}
 
 	&::after {
 		content: "";
-        background-color: var(--white);
-        @include sizing(2rem, 2rem);
+		background-color: var(--white);
+		@include sizing(2rem, 2rem);
 		position: absolute;
-        right: 0.35rem;
-        top: -0.8rem;
+		right: 0.35rem;
+		top: -0.8rem;
 		transform: rotate(45deg);
 		z-index: 0;
+	}
+
+	.details {
+		.title {
+			@include ellipsis;
+		}
+	}
+
+	.bar {
+		@include sizing(1.3rem, 1.3rem);
+
+		span {
+			@include sizing(0.3rem, 100%);
+			background-color: var(--text-color);
+			border-radius: 0.3rem;
+			animation: bar 2.2s ease infinite alternate;
+
+			&:nth-of-type(2) {
+				animation-delay: -2.2s;
+			}
+
+			&:nth-of-type(3) {
+				animation-delay: -3.7s;
+			}
+		}
 	}
 }
 
 @keyframes ellastic {
 	0% {
-        opacity: 0;
+		opacity: 0;
 		transform: scale(1, 1);
 	}
 	10% {
@@ -123,8 +157,30 @@ onMounted(async () => {
 		transform: scale(1.05, 0.95);
 	}
 	100% {
-        opacity: 1;
+		opacity: 1;
 		transform: scale(1, 1);
+	}
+}
+
+@keyframes bar {
+	10% {
+		transform: scaleY(0.3);
+	}
+
+	30% {
+		transform: scaleY(1);
+	}
+
+	60% {
+		transform: scaleY(0.5);
+	}
+
+	80% {
+		transform: scaleY(0.75);
+	}
+
+	100% {
+		transform: scaleY(0.6);
 	}
 }
 </style>
--- server/api/playing.get.ts
+++ server/api/playing.get.ts
@@ -1,25 +1,47 @@
 export default defineEventHandler(async () => {
-    const accessToken = process.env.ACCESS_TOKEN;
-    
-    try {
-        const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        });
-        const data = await response.json();
-        
-        return {
-            isPlaying: true,
-            data: {
-                title: data.item.name,
-                link: data.item.external_urls.spotify,
-                cover_art: data.item.album.images[2].url,
-                l_cover_art: data.item.album.images[1].url,
-                artists: data.item.artists.map((artist: any) => artist.name).join(', '),
-            },
+    const clientId = process.env.CLIENT_ID;
+    const clientSecret = process.env.CLIENT_SECRET;
+    const tokenUrl = process.env.TOKEN_URL!;
+    const refreshToken = process.env.REFRESH_TOKEN!;
+
+	try {
+        const getAccessToken = async () => {
+            const response = await fetch(tokenUrl, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+                },
+                body: new URLSearchParams({
+                    grant_type: 'refresh_token',
+                    refresh_token: refreshToken
+                }),
+            });
+            const data = await response.json();
+            return data.access_token;
         };
-    } catch {
+
+        const apiToken = await getAccessToken();
+
+		const response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+			headers: {
+				Authorization: `Bearer ${apiToken}`,
+			},
+		});
+
+		const data = await response.json();
+        
+		return {
+			isPlaying: true,
+			data: {
+				title: data.item.name,
+				link: data.item.external_urls.spotify,
+				cover_art: data.item.album.images[2].url,
+				l_cover_art: data.item.album.images[1].url,
+				artists: data.item.artists.map((artist: any) => artist.name).join(", "),
+			},
+		};
+	} catch {
         return { isPlaying: false };
-    }
+	}
 });
