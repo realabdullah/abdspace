da631b60ed546baf4070cc4aaebf146e3fdc4e7f
Abdullahi Odesanmi <56407386+realabdullah@users.noreply.github.com>
2025-01-18 15:40:28 +0100

    chore(app): initial commit by Nuxt Studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,48 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	app: {
-		head: {
-			htmlAttrs: {
-				lang: "en",
-			},
-			meta: [
-				{
-					name: "viewport",
-					content: "width=device-width, initial-scale=1",
-				},
-				{
-					charset: "utf-8",
-				},
-			],
-		},
-	},
-	css: ["~/assets/sass/main.scss"],
-	vite: {
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: `
-						@import "@/assets/sass/abstracts/_variables.scss";
-						@import "@/assets/sass/abstracts/_mixins.scss";
-					`,
-				},
-			},
-		},
-	},
-	runtimeConfig: {
-		public: {
-			baseUrl: process.env.SITE_URL ?? "http://localhost:2000",
-		},
-	},
-	devServer: {
-		port: 2000,
-	},
-	imports: {
-		dirs: ["types/**/*"],
-	},
-	devtools: { enabled: false },
-	modules: ["@nuxtjs/supabase", "@nuxt/content"],
-	supabase: {
-		redirect: false,
-	},
-});
+  modules: [
+    '@nuxt/content',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+})
